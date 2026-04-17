2f095a5cf7350e19fbd784c4628b8a66f1e186e6
glennfb15 <glennbitely@icloud.com>
2026-04-17 12:42:20 -0400

    Add initial games array with sample game objects

---
+++ games.js
@@ -0,0 +1,29 @@
+name=games.js
+// Add your games here - just add a new object to this array!
+const games = [
+    {
+        id: 'game1',
+        title: 'Game 1',
+        description: 'Description of your first game',
+        url: 'REPLACE_WITH_EMBEDDED_URL_1'
+    },
+    {
+        id: 'game2',
+        title: 'Game 2',
+        description: 'Description of your second game',
+        url: 'REPLACE_WITH_EMBEDDED_URL_2'
+    },
+    {
+        id: 'game3',
+        title: 'Game 3',
+        description: 'Description of your third game',
+        url: 'REPLACE_WITH_EMBEDDED_URL_3'
+    }
+    // Add more games like this:
+    // {
+    //     id: 'game4',
+    //     title: 'Game 4',
+    //     description: 'Description here',
+    //     url: 'https://your-embedded-game-url.com'
+    // }
+];
